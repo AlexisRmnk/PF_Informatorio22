e5b5a4132fefe4d8ee913c80f86b4605d5d234de
Alexis Romaniuk <alexisromaniuk@gmail.com>
2022-08-10 23:57:53 -0300

    Actualizacion de archivos a clase del martes 09.08.2022

--- apps/noticias/models.py
+++ apps/noticias/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length = 60)
+    descripcion = models.CharField(max_length = 250, null = True, blank = True)
+    
+    def __str__(self) -> str:
+        return self.nombre
+
+class Noticia(models.Model):
+    titulo = models.CharField(max_length = 120)
+    creado = models.DateField(auto_now_add = True)
+    cuerpo = models.TextField()
+    autor = models.CharField(max_length = 50, null = True, blank = True)
+    imagen = models.ImageField(upload_to = 'noticias', null = True, blank = True)
+    # 'null' es para que la BD acepte valores nulos. 'Blank' es para que el 
+    # formulario (la entrada) permita valores VACIOS. Van los dos juntos.
+    
+    # la 'Noticia' va a tener un campo 'categoria' que va a ser una clave
+    # foranea a la clase 'Categoria'. On_delete CASCADE  indica que si se borra
+    # una categoria, automaticamente se van a borrar todas las notis de esa cat.
+    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE, 
+                                  null = True)
+    # relacion   >>>   CATEGORIA    1:N     NOTICIA
+    
+    def __str__(self) -> str:
+        return self.titulo
+    
--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -6,5 +6,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
-    # aca vamos a especificar las caracteristucas especificas del modelo de user.
+    # aca vamos a especificar las caracteristicas especificas del modelo de
+    # user.
     pass
