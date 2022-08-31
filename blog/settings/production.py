e90471ec2bd072c190f8408b1440300a50238e99
Ricardo <ricagorostizu@gmail.com>
2022-08-31 17:15:55 -0300

    heroku 3

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -1,1 +1,24 @@
-# vacio hasta que subamos la APP a internet
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-2awi$098mo1txo-f%)ns!3@7h++aph&u1tc8vjm=237=y)z6i3'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+
+# Esto es para conectar a la BBDD de sqlite. Es mas ligero y simple. 
+# NO SE INSTALA. Solo se usa en entornos de PRUEBA
+# Es mas facil de compartir por GIT paraque todos los miembros de un grupo
+# trabajan sobre lo mismo. 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
