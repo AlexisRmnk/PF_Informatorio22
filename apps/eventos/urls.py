fcc37745d2cf3d441c24af290e88844fae5d4507
Alexis Romaniuk <alexisromaniuk@gmail.com>
2022-09-01 16:35:44 -0300

    integrado calendario funcional

--- apps/eventos/urls.py
+++ apps/eventos/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('listar/', views.listar, name = 'listar_eventos'),
     path('detalle/<int:pk>', views.DetalleEvento.as_view(), 
          name = 'detalle_eventos'),
-    path('calendario/', views.Calendario, name = 'calendario'),
+    path('calendario/', views.CalendarView.as_view(), name = 'calendario'),
 ]
--- apps/eventos/views.py
+++ apps/eventos/views.py
@@ -4,7 +4,10 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin # para clases
 from django.core.paginator import Paginator
-# Create your views here.
+from django.utils.safestring import mark_safe
+from .utils import Calendar
+from datetime import datetime, date, timedelta
+import calendar
 
 #UNUSED
 #class ListaEvento(LoginRequiredMixin, ListView
@@ -52,5 +55,31 @@ class DetalleEvento(DetailView):
     context_object_name = "evento"
     template_name = 'eventos/detalle_evento.html'
     
-def Calendario(request):
+def Calendario(request): #del calendario sin back
     return render(request, 'eventos/calendario.html')
+
+class CalendarView(ListView):
+    model = Evento
+    template_name = 'eventos/calendarioV2.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # use today's date for the calendar
+        d = get_date(self.request.GET.get('day', None))
+
+        # Instantiate our calendar class with today's year and date
+        cal = Calendar(d.year, d.month)
+
+        # Call the formatmonth method, which returns our calendar as a table
+        html_cal = cal.formatmonth(withyear=True)
+        d = get_date(self.request.GET.get('month', None))
+        context['calendar'] = mark_safe(html_cal)
+        return context
+
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
+
