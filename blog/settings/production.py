2d8abe3fb4edcaea922814707352e09e37198f77
Ricardo <ricagorostizu@gmail.com>
2022-08-31 21:40:46 -0300

    deploy heroku v2

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-2awi$098mo1txo-f%)ns!3@7h++aph&u1tc8vjm=237=y)z6i3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://informatorio2022.herokuapp.com/']
+ALLOWED_HOSTS = ['informatorio2022.herokuapp.com']
 
 
 
